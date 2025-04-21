847cde5a762089ac3b9ff8e43f8ddf9d04b7496c
Gustavo Bomfim dos Santos <126712923+GuSantoz@users.noreply.github.com>
2025-04-21 11:47:05 -0300

    Create  1008 - Salário.py

---
+++ INICIANTE/1008 - Salário.py
@@ -0,0 +1,9 @@
+number = int(input())
+horas_trabalhadas = int(input())
+recebe_hora = float(input())
+
+
+salario = horas_trabalhadas * recebe_hora
+
+print(f'NUMBER = {number}')
+print(f'SALARY = U$ {salario:.2f}')
