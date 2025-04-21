c0c3dee247c9dcfe4c333e7f8fe43c8bbb6fa488
Gustavo Bomfim dos Santos <126712923+GuSantoz@users.noreply.github.com>
2025-04-21 12:02:55 -0300

    Create  1546 - Feedback.py

---
+++ INICIANTE/1546 - Feedback.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+for _ in range(N):
+    K = int(input())
+    
+    for _ in range(K):
+            f = int(input())
+            if f == 1:
+                print('Rolien')
+            elif f == 2:
+                print('Naej')
+            elif f == 3:
+                print('Elehcim')
+            elif f == 4:
+                print('Odranoel')    
